487b3498ff999696b0d6e188b31d02b15dd3cf19
YuvalBansal <yuvalbansal200.yb@gmail.com>
2024-01-30 15:47:41 +0530

    fixed bugs

--- Shooter_Game.ino
+++ Shooter_Game.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+const int buzzerPin = 2;
+const int potentiometerPIN = 0;
+const int buttonPin = 3;
+const int ledPin = 4;
 
+int buttonState = 0;  // variable for reading the pushbutton status
+int value = 0;
+
+void setup(){ 
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+  value = analogRead(potentiometerPIN);
+  Serial.print("Sensor Value: ");
+  Serial.println(value);
+  tone(buzzerPin, value/2, 500);
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+  delay(100);
 }
